38c036f39dfa7832fd77fc853fdde2e474884825
Karen Byrd <klbyrd88@gmail.com>
2021-07-07 09:36:24 -0500

    styled learn and free buttons

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,8 +10,8 @@ class Hero extends React.Component {
         <Navbar />
         <img src={devices} alt="Electronic Screens" />
         <Title>The Time has come</Title>
-        <button>Try for Free</button>
-        <button>Learn More</button>
+        <Free>Try for Free</Free>
+        <Learn>Learn More</Learn>
       </div>
     );
   }
@@ -22,4 +22,20 @@ export default Hero;
 const Title = styled.h1`
   width: 50%;
   text-align: center;
+`;
+
+const Free = styled.button`
+  background-color: #111b47;
+  color: #fff;
+  width: 11.5rem;
+  height: 2.2rem;
+  margin-left: 10rem;
+`;
+
+const Learn = styled.button`
+  background: transparent;
+  color: #111b47;
+  width: 11.5rem;
+  height: 2.2rem;
+  margin-left: 2rem;
 `;
