8a3557a54c4ff07e8384fe87bc3f9776059b3008
kjcarter-design <k.carter.design@icloud.com>
2021-07-19 11:57:15 -0400

    troubleshooting

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -10,13 +10,13 @@ class Reviews extends React.Component {
       <Container>
         <ReviewCarousel>
           <Review>
-            <OpenQuotation src={OpenQuote} />
+            <OpenQuotation src={OpenQuote}  alt='Open Quote'/>
             <ReviewText>
               Lore brings the fight to your fingers. Fight with legends and
               explore new lands. Each quest requires speed, knowledge but most
               importantly it requires heart.
             </ReviewText>
-            <CloseQuotation src={CloseQuote} />
+            <CloseQuotation src={CloseQuote} alt='Close Quote' />
 
             <Attribution>
               <p> -- John Smith</p>
